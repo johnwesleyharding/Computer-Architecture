d451ae5b041abc42309f64097ae93c5d527a3ee8
johnwesleyharding <johnwesleyharding@users.noreply.github.com>
2020-05-18 19:42:37 -0600

    _

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,20 +1,16 @@
-"""CPU functionality."""
 
 import sys
 
 class CPU:
-    """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+
+        self.reg = {'pc': 0, 'ir': 0, 'mar': 0, 'mdr': 0, 'fl': 0}
+        self.ram = [0] * 256
 
     def load(self):
-        """Load a program into memory."""
 
         address = 0
-
-        # For now, we've just hardcoded a program:
 
         program = [
             # From print8.ls8
@@ -30,9 +26,7 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
@@ -41,10 +35,6 @@ class CPU:
             raise Exception("Unsupported ALU operation")
 
     def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
@@ -53,13 +43,12 @@ class CPU:
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
-        ), end='')
+        ), end = '')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.reg[i], end = '')
 
         print()
 
     def run(self):
-        """Run the CPU."""
         pass
